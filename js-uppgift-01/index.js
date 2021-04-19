4538f2600985afee5b61061e4bba624a7946bd75
conorhar <conorhar.91@gmail.com>
2021-04-19 16:13:20 +0200

    changes

--- js-uppgift-01/index.js
+++ js-uppgift-01/index.js
@@ -1,12 +1,12 @@
 //Retrieves product array with fetch API (Mahmud's replacement API)
-// fetch('https://webacademy.se/fakestore/')
-//             .then(res=>res.json())
-//             .then(data=>renderProducts(data))
-
-//Original API (dead link)
-fetch('https://fakestoreapi.com/products')
+fetch('https://webacademy.se/fakestore/')
             .then(res=>res.json())
             .then(data=>renderProducts(data))
+
+//Original API (dead link)
+// fetch('https://fakestoreapi.com/products')
+//             .then(res=>res.json())
+//             .then(data=>renderProducts(data))
 
 renderCartQuantity();
 
